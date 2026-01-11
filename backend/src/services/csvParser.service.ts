f9844c0c36e0dc9bed693cc6b24232d33275d229
smundhra <smundhra@konradgroup.com>
2026-01-11 16:22:47 +0530

    Updated the csv parsing.

--- backend/src/services/csvParser.service.ts
+++ backend/src/services/csvParser.service.ts
@@ -2,31 +2,6 @@ import csv from "csv-parser";
 import { validateDate } from "../utils/validateDate.ts";
 import { Readable } from "stream";
 
-// export const parseCSV = async (buffer: Buffer) => {
-//     // Array to store each rows transaction data
-//     const results: any[] = [];
-//     // Create a readable stream from the buffer
-//     const stream = Readable.from(buffer.toString());
-//     // Return a promise that resolves when parsing is complete
-//     // Streams are event-based, so we use a promise to handle async flow
-//     return new Promise<any[]>((resolve,reject)=> {
-//         stream.pipe(csv())
-//               .on("data",(row)=>{
-                
-//                 results.push({
-//                     txnDate: row['DATE'],
-//                     narration: row['NARRATION'],
-//                     debit: row["WITHDRAWAL (DR)"] ? Number(row["WITHDRAWAL (DR)"]) : null,
-//                     credit: row["DEPOSIT (CR)"] ? Number(row["DEPOSIT (CR)"]) : null,
-//                     balance: Number(row["BALANCE"]),
-//                     source: "csv",
-//                 });
-//               })
-//               .on("end",()=> resolve(results))
-//               .on("error",(error)=> reject(error));        
-//     });
-// };
-
 export const parseCSV = async (buffer: Buffer) => {
   const results: any[] = [];
   const stream = Readable.from(buffer.toString());
@@ -59,13 +34,13 @@ export const parseCSV = async (buffer: Buffer) => {
         if (!validateDate(row.date)) return;
 
         const debit = parseFloat(
-          (row.debit || "").replace(/[$,\s]/g, "")
+          (row.debit || "").replace(/[,\s]/g, "")
         );
         const credit = parseFloat(
-          (row.credit || "").replace(/[$,\s]/g, "")
+          (row.credit || "").replace(/[,\s]/g, "")
         );
         const balance = parseFloat(
-          (row.balance || "").replace(/[$,\s]/g, "")
+          (row.balance || "").replace(/[,\s]/g, "")
         );
 
         results.push({
