6b0160d5fd7d86101b93385b006ea30a6966ac73
smundhra <smundhra@konradgroup.com>
2026-01-12 11:35:55 +0530

    UI creation and file upload feature

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from "cors"
 import connectDB from './config/db-test.js';
 import uploadRouter from './routes/upload.routes.ts';
 
@@ -9,6 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(uploadRouter)
 
